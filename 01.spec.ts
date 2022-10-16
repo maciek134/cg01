eb0f54695cd35f22b3305a116c14fd28ec66617e
Maciej Sopylo <me@klh.io>
2022-10-16 22:57:04 +0200

    feat: implement flattening function

--- 01.spec.ts
+++ 01.spec.ts
@@ -4,14 +4,19 @@ import { flatten } from './01';
 describe('01', () => {
   it('returns a flattened array', () => {
     const result = flatten([ 1, 2, [ 3, 4 ], 5, [ 6, 7 ]]);
-    expect(result).to.equal([ 1, 2, 3, 4, 5, 6, 7 ]);
+    expect(result).to.deep.equal([ 1, 2, 3, 4, 5, 6, 7 ]);
   });
 
   it('throws on circular input', () => {
     const a: any[] = [ 1, 2, 3 ];
     const b = [ 4, 5, a ];
-    a[3] = b;
+    const c = [ 0, b, 0 ]
+    a[3] = c;
 
-    expect(flatten.bind(null, a)).to.throw(new TypeError('input is circular'));
+    function testFlatten() {
+      flatten(a);
+    }
+
+    expect(testFlatten).to.throw(TypeError, 'input is circular');
   });
 });
--- 01.ts
+++ 01.ts
@@ -1,4 +1,30 @@
-//TODO: add proper types
+/**
+ * Performs the flattening while checking the `parents` WeakSet for circularity
+ * 
+ * @param input the array to flatten
+ * @param parents internal array references
+ * @returns flat array
+ */
+function flattenInternal(input: any[], parents: WeakSet<any>): any[] {
+  const ret: any[] = [];
+
+  for (const value of input) {
+    if (!Array.isArray(value)) {
+      ret.push(value);
+      continue;
+    }
+
+    if (parents.has(value)) {
+      throw new TypeError('input is circular');
+    }
+
+    parents.add(value);
+    ret.push(...flattenInternal(value, parents));
+  }
+
+  return ret;
+}
+
 export function flatten(input: any[]): any[] {
-  return input;
+  return flattenInternal(input, new WeakSet());
 }
